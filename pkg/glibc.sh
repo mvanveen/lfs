7efa353cb547acdb769d2ac97a98283a259d270c
Michael Van Veen <michael@mvanveen.net>
2020-07-03 01:08:32 -0700

    glibc changes

--- pkg/glibc.sh
+++ pkg/glibc.sh
@@ -1,3 +1,22 @@
 cd /mnt/lfs/sources
 
+rm -rf xf glibc-2.31
+tar xf glibc-2.31.tar.xz
+cd glibc-2.31
 
+mkdir -v build
+cd build
+
+mkdir tmp/
+
+../configure                             \
+      TMP_DIR=tmp/                       \
+      --prefix=/tools                    \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=3.2                \
+      --with-headers=/tools/include
+
+make -j1
+
+make install
